2e50eb95989f0ce436be6936204a40479f6ee404
TaimurHasan <taimurhasan11@gmail.com>
2022-05-21 17:43:55 -0400

    added homepage frontend

---
+++ controllers/home-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models')
+
+router.get('/', (req, res) => {
+    // find all posts to display on home page
+    Post.findAll({
+        attributes: ['id', 'title', 'content', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // serialize post data
+        const posts = dbPostData.map(post => post.get({ plain: true }))
+
+        // render homepage handlebars template with posts
+        res.render('homepage', { posts })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+// handlebars template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 // instantialize express
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // set static folder for front end
 app.use(express.static(path.join(__dirname, 'public')));
+
+// set handlebars as template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // use routes
 app.use(routes);
