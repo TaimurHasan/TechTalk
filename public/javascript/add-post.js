29386e432bde3b1f3f18cdb2735bdf5fbd08e626
TaimurHasan <taimurhasan11@gmail.com>
2022-05-23 09:48:08 -0400

    adding format_plural function and changing color scheme

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,22 +4,26 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('textarea[name="post-body"]').value;
 
-    const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            content
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    if(title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-
+        
     if (response.ok) {
         document.location.replace('/profile');
     } else {
         alert(response.statusText);
+    }
+    } else {
+        alert("Please fill out all the details!")
     }
 }
 
